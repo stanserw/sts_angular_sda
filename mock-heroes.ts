5c3dfee2ff37c7bd192fbfa6ca1e4a7f3ad129f7
Stanisław Serwin <[stanserw@wp.pl]>
2018-11-09 20:45:25 +0100

    toh-pt6

--- mock-heroes.ts
+++ mock-heroes.ts
@@ -1,5 +1,4 @@
 import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
 
 export const HEROES: Hero[] = [
   { id: 11, name: 'Mr. Nice' },
@@ -13,4 +12,3 @@ export const HEROES: Hero[] = [
   { id: 19, name: 'Magma' },
   { id: 20, name: 'Tornado' }
 ];
-
